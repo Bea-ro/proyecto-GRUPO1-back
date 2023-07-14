8aabb390c1a6bdd9c6f49161c34f435dd75930ec
Rubcs <Rubcs@gmail.com>
2023-07-14 13:56:06 +0200

    config cors and rate limiter

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
-const express = require('express');
-const { connectDB } = require('./src/utils/connectDB');
-require('dotenv').config();
-const mainRouter = require('./src/api/routes/index.routes');
-const { configCloudinary } = require('./src/api/middlewares/uploadImg');
+const express = require("express");
+const { connectDB } = require("./src/utils/connectDB");
+require("dotenv").config();
+const mainRouter = require("./src/api/routes/index.routes");
+const { configCloudinary } = require("./src/api/middlewares/uploadImg");
 
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
 const server = express(); //podría ser tb const server = express()
-server.use(express.json({limit: '5mb'})) 
+server.use(express.json({ limit: "5mb" }));
 server.use(express.urlencoded({ limit: "5mb", extended: false }));
 
-configCloudinary()
+configCloudinary();
 connectDB();
 
-server.use((req, res, next) => { //define métodos y tipo de contenidos aceptados como requests
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-  res.header('Access-Control-Allow-Headers', 'Content-type')
-next()
-})
-server.disable('x-powered-by') //Quita de los header info con qué está hecha la api (Express)
+server.use(cors());
 
-server.use('/api', mainRouter)
+const limiter = rateLimit({
+  windowMs: 3 * 60 * 1000,
+  max: 50,
+  standardHeaders: false,
+  legacyHeaders: false,
+});
 
-server.use('*', (req, res, next) => {
-    res.status(404).json({data: 'Not found'})
-})
+server.use(limiter);
+
+server.use((req, res, next) => {
+  //define métodos y tipo de contenidos aceptados como requests
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-type");
+  next();
+});
+server.disable("x-powered-by"); //Quita de los header info con que está hecha la api (Express)
+
+server.use("/api", mainRouter);
+
+server.use("*", (req, res, next) => {
+  res.status(404).json({ data: "Not found" });
+});
 
 server.use((error, req, res, next) => {
-    res.status(500).json({data: 'Internal Server Error'})
-})
+  res.status(500).json({ data: "Internal Server Error" });
+});
 
 server.listen(3001, () => {
-console.log(`la aplicación está corriendo en el puerto http:localhost:${process.env.PORT}`)
-})
+  console.log(
+    `la aplicación está corriendo en el puerto http:localhost:${process.env.PORT}`
+  );
+});
--- src/api/routes/destinations.routes.js
+++ src/api/routes/destinations.routes.js
@@ -1,23 +1,25 @@
-const express = require('express');
+const express = require("express");
 const {
   getAllDestinations,
   getDestinationById,
   createDestination,
   updateDestination,
   deleteDestination,
-  uploadDestinationImg
-} = require('../controllers/destinations.controllers');
-const { uploadImgCloudinary } = require('../middlewares/uploadImg');
-const { isAuth } = require('../middlewares/isAuth.middleware');
+  uploadDestinationImg,
+} = require("../controllers/destinations.controllers");
+const { uploadImgCloudinary } = require("../middlewares/uploadImg");
+const { isAuth } = require("../middlewares/isAuth.middleware");
 const destinationsRouter = express.Router();
 
-
-
-destinationsRouter.get('/', [isAuth], getAllDestinations);
-destinationsRouter.get('/:id', getDestinationById);
-destinationsRouter.post('/', createDestination);
-destinationsRouter.put('/:id', updateDestination);
-destinationsRouter.patch('/:id', uploadImgCloudinary.single('images'), uploadDestinationImg);
-destinationsRouter.delete('/:id', deleteDestination);
+destinationsRouter.get("/", getAllDestinations);
+destinationsRouter.get("/:id", getDestinationById);
+destinationsRouter.post("/", createDestination);
+destinationsRouter.put("/:id", updateDestination);
+destinationsRouter.patch(
+  "/:id",
+  uploadImgCloudinary.single("images"),
+  uploadDestinationImg
+);
+destinationsRouter.delete("/:id", deleteDestination);
 
 module.exports = destinationsRouter;
